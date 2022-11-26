482d922ae5b354e122c976512a30eb5726d9d3f9
Xavier Hernandez <xtriminioh@gmail.com>
2022-11-26 09:57:43 -0600

    agregamos el documento Dockerfile para configurar crear la imagen, y algunos archivos del la appweb

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM alpine:3.17
 
-RUN apk add python3.7
+RUN apk add python3-dev
+
---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+        DATABASE=os.path.join(app.instance_path,'app.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #routes
+    @app.route('/')
+    def home():
+        return 'Hola'
+
+    return app
